d8ff7a34ff82ba7e61bbaa2f8809fc7e50130ba8
Eldar <65613517+the-Eldar@users.noreply.github.com>
2020-06-17 00:41:41 +0500

    Update snakeGame.js

--- snakeGame.js
+++ snakeGame.js
@@ -3,9 +3,10 @@ const canvas = document.querySelector(".cnvs"),
   foodSnake = new Image();
 foodSnake.src = "food.png";
 
-let box = 32;
+let box = 24;
 
 let score = 0;
+let count = document.querySelector(".count");
 
 let food = {
   x: Math.floor(Math.random() * 25) * box,
@@ -15,9 +16,25 @@ let food = {
 let snake = [];
 
 snake[0] = {
-  x: 300,
+  x: 280,
   y: 280,
 };
+
+document.addEventListener("keydown", direcion);
+
+let dir;
+
+function direcion(event) {
+  if (event.keyCode == 37 && dir != "right") {
+    dir = "left";
+  } else if (event.keyCode == 38 && dir != "down") {
+    dir = "up";
+  } else if (event.keyCode == 39 && dir != "left") {
+    dir = "right";
+  } else if (event.keyCode == 40 && dir != "up") {
+    dir = "down";
+  }
+}
 
 function draw() {
   ctx.drawImage(foodSnake, food.x, food.y);
@@ -26,5 +43,29 @@ function draw() {
     ctx.fillStyle = " black";
     ctx.fillRect(snake[i].x, snake[i].y, box, box);
   }
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  snake.pop();
+
+  if (dir == "left") {
+    snakeX -= box;
+  }
+  if (dir == "right") {
+    snakeX += box;
+  }
+  if (dir == "up") {
+    snakeY -= box;
+  }
+  if (dir == "down") {
+    snakeY += box;
+  }
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
 }
 let game = setInterval(draw, 100);

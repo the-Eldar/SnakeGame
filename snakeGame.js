24aba8b30c2fd74535c2382d47384b000bbc050c
Eldar <65613517+the-Eldar@users.noreply.github.com>
2020-06-17 22:57:16 +0500

    Update snakeGame.js

--- snakeGame.js
+++ snakeGame.js
@@ -9,22 +9,22 @@ let score = 0;
 let count = document.querySelector(".count");
 
 let food = {
-  x: Math.floor(Math.random() * 25) * box,
-  y: Math.floor(Math.random() * 25) * box,
+  x: Math.floor(Math.random() * 17) * box,
+  y: Math.floor(Math.random() * 15) * box,
 };
 
 let snake = [];
 
 snake[0] = {
-  x: 280,
-  y: 280,
+  x: 17 * box,
+  y: 15 * box,
 };
 
-document.addEventListener("keydown", direcion);
+document.addEventListener("keydown", direction);
 
 let dir;
 
-function direcion(event) {
+function direction(event) {
   if (event.keyCode == 37 && dir != "right") {
     dir = "left";
   } else if (event.keyCode == 38 && dir != "down") {
@@ -40,13 +40,25 @@ function draw() {
   ctx.drawImage(foodSnake, food.x, food.y);
 
   for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = " black";
+    if (i == 0) {
+      ctx.fillStyle = " black";
+    } else {
+      ctx.fillStyle = " grey";
+    }
     ctx.fillRect(snake[i].x, snake[i].y, box, box);
   }
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  snake.pop();
+  if (snakeX == food.x && snakeY == food.y) {
+    score++;
+    food = {
+      x: Math.floor(Math.random() * 25) * box,
+      y: Math.floor(Math.random() * 25) * box,
+    };
+  } else {
+    snake.pop();
+  }
 
   if (dir == "left") {
     snakeX -= box;
@@ -69,3 +81,4 @@ function draw() {
   snake.unshift(newHead);
 }
 let game = setInterval(draw, 100);
+console.log(score);
